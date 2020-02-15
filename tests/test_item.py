7cb32a78ead0ccbd0f19c7b9aa66984ec6acb0ac
artcom-net <kust.artcom@gmail.com>
2020-02-15 23:32:15 +0300

    Add item tests

---
+++ tests/test_item.py
@@ -0,0 +1,35 @@
+import pytest
+
+
+def _get_item_names(items):
+    return [item.name for item in items]
+
+
+@pytest.fixture
+def d2s_items(d2s_file):
+    d2s, _ = d2s_file
+    return d2s.items
+
+
+@pytest.fixture
+def stash_items(stash_file):
+    stash, _ = stash_file
+    return [item for page in stash.stash for item in page['items']]
+
+
+@pytest.fixture
+def d2s_item_names(d2s_items):
+    return _get_item_names(d2s_items)
+
+
+@pytest.fixture
+def stash_item_names(stash_items):
+    return _get_item_names(stash_items)
+
+
+def test_d2s_item_name(d2s_item_names):
+    assert all(d2s_item_names)
+
+
+def test_stash_item_name(stash_item_names):
+    assert all(stash_item_names)
