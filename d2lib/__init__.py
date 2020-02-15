603a0cef81faf40f0075abde61e558a680d5599f
artcom-net <kust.artcom@gmail.com>
2020-02-15 23:39:23 +0300

    Change the version to 0.2.4

--- d2lib/__init__.py
+++ d2lib/__init__.py
@@ -8,7 +8,7 @@ Currently supported:
 """
 
 __name__ = 'd2lib'
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 __author__ = 'Artem Kustov'
 __author_email__ = 'kust.artcom@gmail.com'
 __license__ = 'MIT'
