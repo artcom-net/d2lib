87fafb6b9523899474fdadb6656042ed4b07e99e
artcom-net <kust.artcom@gmail.com>
2020-08-01 01:24:49 +0300

    Add a base class for exceptions

--- d2lib/errors.py
+++ d2lib/errors.py
@@ -1,16 +1,21 @@
-class D2SFileParseError(Exception):
+class _BaseParseException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class D2SFileParseError(_BaseParseException):
     """Used to parse .d2s files when something went wrong."""
 
     pass
 
 
-class StashFileParseError(Exception):
+class StashFileParseError(_BaseParseException):
     """Used to parse .d2x and .sss files when something went wrong."""
 
     pass
 
 
-class ItemParseError(Exception):
+class ItemParseError(_BaseParseException):
     """Used to parse item structure when something went wrong."""
 
     pass
